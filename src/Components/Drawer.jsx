a2c806e5820f745dce2f1669bf370d58fc76ab5d
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-07-29 00:09:42 +0545

    fix bg svg

--- src/Components/Drawer.jsx
+++ src/Components/Drawer.jsx
@@ -9,7 +9,7 @@ import ThemeMode from './ThemeMode';
 
 const Drawer = ({ darkMode }) => {
   return (
-    <div className="drawer drawer-end ">
+    <div className="drawer drawer-end z-10">
       <input id="my-drawer-4" type="checkbox" className="drawer-toggle" />
       <div className="drawer-content ">
         {/* Page content here */}
