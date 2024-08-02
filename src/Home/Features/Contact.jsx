ae4b729284c8808a6969a1400855ada30e06ec35
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-08-02 15:14:48 +0545

    dd

--- src/Home/Features/Contact.jsx
+++ src/Home/Features/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = ({ darkMode }) => {
           <NavLink to='#' className={`flex justify-between p-4 ${darkMode ? 'bg-gray-700' : 'bg-gray-200'}`}>
             <div className='flex gap-4'>
               <img src={mail} alt="mail" />
-              <span>contact@cobrand.com.np</span>
+              <span className='text-sm md:text-md '>contact@cobrand.com.np</span>
             </div>
             <img src={arrow} alt="arrow" />
           </NavLink>
@@ -85,7 +85,7 @@ const Contact = ({ darkMode }) => {
 
 
         <section>
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3538.4000503366747!2d83.4524294743311!3d27.519027976291092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48c91b103b2e71fd%3A0x619e0f7f7ca77354!2sCoBrand%20Digital!5e0!3m2!1sen!2snp!4v1722337453868!5m2!1sen!2snp" height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3538.4000503366747!2d83.4524294743311!3d27.519027976291092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48c91b103b2e71fd%3A0x619e0f7f7ca77354!2sCoBrand%20Digital!5e0!3m2!1sen!2snp!4v1722337453868!5m2!1sen!2snp" height="450" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
         </section>
       </div>
 
